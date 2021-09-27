3f8aa06979fc61436acdd200214df05ee259510c
Natalia Tyshchenko <nataly_kt@hotmail.com>
2021-09-27 14:59:20 +0300

    Project2 - Step2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,10 +7,13 @@ const program = new commander.Command();
 program
   .version('1.0.0', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish');
 /*
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
   */
+
 program.parse(process.argv);
