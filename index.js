63ea340c3b4645ed128d192f4e2fb4a1fb3c07ba
Natalia Tyshchenko <nataly_kt@hotmail.com>
2021-10-06 13:12:09 +0300

    modified: parsers

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import fs from 'fs';
+import path from 'path';
 import _ from 'lodash';
 
+const getDataFromJson = (filepath) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'));
+/* Было: 
 const getDataFromJson = (filepath) => JSON.parse(fs.readFileSync(filepath, 'utf-8'));
+Добавлен поиск относит. путей
+*/
+
+// Или доработать, или убрать 
+/* 
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath); 
+const getFormat = (filepath) => path.extname(filepath).substr(1);
+const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
+const parsers = {json: JSON.parse,};
+const parse = (data, format) => parsers[format](data);
+
+const getDataFromJson = (anyFileName) => {
+const filepath = getFullPath(anyFileName);
+return parse(readFile(filepath), getFormat(filepath));
+};
+*/
 
 const genDiff = (data1, data2) => {
   const keys1 = Object.keys(data1);
