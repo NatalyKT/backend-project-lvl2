044e4616464122e0e2a4cd580b297c5b41663830
Natalia Tyshchenko <nataly_kt@hotmail.com>
2021-11-10 14:26:58 +0300

    fix bugs in  gendiff.js

--- Makefile
+++ Makefile
@@ -13,4 +13,13 @@ test:
 test-coverage:
 	npm test -- --coverage --coverageProvider=v8
 
+stylish:
+	gendiff -f stylish __fixtures__/file1.json __fixtures__/file2.json
+
+plain:
+	gendiff -f plain __fixtures__/file1.json __fixtures__/file2.json
+
+json:
+	gendiff -f json __fixtures__/file1.json __fixtures__/file2.json
+
 .PHONY: test
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-import program from 'commander';
+// import program from 'commander';
+import { Command } from 'commander';
+import process from 'process';
 import genDiff from '../index.js';
+
+const program = new Command();
 
 program
   .version('1.0.0')
@@ -8,7 +12,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2, program.format));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
     // или:
   /*
   const diff = genDiff(filePath1, filePath2, program.format);
