fbb4f0fc9f3d27c09df46c2eb62f44a4b17a6b55
Natalia Tyshchenko <nataly_kt@hotmail.com>
2021-11-11 16:05:13 +0300

    refactoring: src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
-import format from '../formatters/index.js';
+import format from './formatters/index.js';
 import parse from './parsers.js';
 
 const buildTree = (data1, data2) => {
@@ -9,7 +9,7 @@ const buildTree = (data1, data2) => {
   const allSortedKeys = _.sortBy(keys);
 
   const result = allSortedKeys.map((key) => {
-    if (!_.has(data1, key) && _.has(data2, key)) {
+    if (!_.has(data1, key) /* && _.has(data2, key) */) {
       return {
         key,
         type: 'added',
@@ -63,14 +63,14 @@ const showDiff = (filePath1, filePath2, outputFormat = 'stylish') => {
   const dataOfFile1 = fs.readFileSync(getFullPath(filePath1), 'utf-8');
   const dataOfFile2 = fs.readFileSync(getFullPath(filePath2), 'utf-8');
 
-  const extensionOfFile1 = path.extname(getFullPath(filePath1)).replace('.', '');
-  const extensionOfFile2 = path.extname(getFullPath(filePath2)).replace('.', '');
+  const dataTypeOfFile1 = path.extname(getFullPath(filePath1)).replace('.', '');
+  const dataTypeOfFile2 = path.extname(getFullPath(filePath2)).replace('.', '');
 
-  const parsedData1 = parse(dataOfFile1, extensionOfFile1);
-  const parsedData2 = parse(dataOfFile2, extensionOfFile2);
+  const parsedData1 = parse(dataOfFile1, dataTypeOfFile1);
+  const parsedData2 = parse(dataOfFile2, dataTypeOfFile2);
 
-  const getDiff = genDiff(parsedData1, parsedData2);
-  const diff = format(getDiff, outputFormat);
+  const formattedDiff = genDiff(parsedData1, parsedData2);
+  const diff = format(formattedDiff, outputFormat);
 
   return diff;
 
