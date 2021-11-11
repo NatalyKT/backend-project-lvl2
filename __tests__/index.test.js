2554ce52ab6a9b74e2efb4c7f9288fc05e1ceccb
Natalia Tyshchenko <nataly_kt@hotmail.com>
2021-11-11 17:05:19 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-commented-out-tests */
 import { test, expect, describe } from '@jest/globals';
 import path from 'path';
 import fs from 'fs';
@@ -10,13 +11,37 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
+const stylishResult = readFixture('expected_stylish.txt');
+const plainResult = readFixture('expected_plain.txt');
+const jsonResult = readFixture('expected_json.txt');
+
+const formats = [
+  'json',
+  'yaml',
+  'yml',
+];
+
+describe.each(formats)('Test genDiff', (format) => {
+  const path1 = getFixturePath(`file1.${format}`);
+  const path2 = getFixturePath(`file2.${format}`);
+
+  test(`${format}`, () => {
+    expect(genDiff(path1, path2)).toEqual(stylishResult);
+    expect(genDiff(path1, path2, 'stylish')).toEqual(stylishResult);
+    expect(genDiff(path1, path2, 'plain')).toEqual(plainResult);
+    expect(genDiff(path1, path2, 'json')).toEqual(jsonResult);
+    expect(() => { JSON.parse(genDiff(path1, path2, 'json')); }).not.toThrow();
+  });
+});
+
+/*
 const outputFormats = ['stylish', 'plain', 'json'];
 const extensions = [
   ['json', 'json'],
   ['yml', 'yml'],
   ['yaml', 'yaml'],
 ];
-
+*/
 // или: вариант "многосторонней" проверки
 /*
 const extensions = [
@@ -30,7 +55,6 @@ const extensions = [
   ['yml', 'json'],
   ['yaml', 'json'],
 ];
-*/
 
 describe('genDiff', () => {
   outputFormats.forEach((outputFormat) => {
@@ -48,3 +72,5 @@ describe('genDiff', () => {
     });
   });
 });
+
+*/
